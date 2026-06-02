ceb2be00f684445a8a463f18a13a0a6160494b8d
Samiksha-bajoria <samikshabajoria2006@gmail.com>
2026-06-02 22:14:26 +0530

    added import

--- Dreamwave Digital/src/lib/voices.ts
+++ Dreamwave Digital/src/lib/voices.ts
@@ -1,3 +1,45 @@
+// Import speaker images
+import speaker1 from "@/assets/speakers/speaker-1.jpg";
+import speaker2 from "@/assets/speakers/speaker-2.jpg";
+import speaker3 from "@/assets/speakers/speaker-3.jpg";
+import speaker4 from "@/assets/speakers/speaker-4.jpg";
+import speaker5 from "@/assets/speakers/speaker-5.jpg";
+import speaker6 from "@/assets/speakers/speaker-6.jpg";
+import speaker7 from "@/assets/speakers/speaker-7.jpg";
+import speaker8 from "@/assets/speakers/speaker-8.jpg";
+import speaker9 from "@/assets/speakers/speaker-9.jpg";
+import speaker10 from "@/assets/speakers/speaker-10.jpg";
+import speaker11 from "@/assets/speakers/speaker-11.jpg";
+import speaker12 from "@/assets/speakers/speaker-12.jpg";
+import speaker13 from "@/assets/speakers/speaker-13.jpg";
+import speaker14 from "@/assets/speakers/speaker-14.jpg";
+import speaker15 from "@/assets/speakers/speaker-15.jpg";
+import speaker16 from "@/assets/speakers/speaker-16.jpg";
+import speaker17 from "@/assets/speakers/speaker-17.jpg";
+import speaker18 from "@/assets/speakers/speaker-18.jpg";
+
+// Import sponsor logos
+import sponsor1 from "@/assets/sponsors/sponsor-1.png";
+import sponsor2 from "@/assets/sponsors/sponsor-2.png";
+import sponsor3 from "@/assets/sponsors/sponsor-3.png";
+import sponsor4 from "@/assets/sponsors/sponsor-4.png";
+import sponsor5 from "@/assets/sponsors/sponsor-5.png";
+import sponsor6 from "@/assets/sponsors/sponsor-6.png";
+import sponsor7 from "@/assets/sponsors/sponsor-7.png";
+import sponsor8 from "@/assets/sponsors/sponsor-8.png";
+import sponsor9 from "@/assets/sponsors/sponsor-9.png";
+import sponsor10 from "@/assets/sponsors/sponsor-10.png";
+import sponsor11 from "@/assets/sponsors/sponsor-11.png";
+import sponsor12 from "@/assets/sponsors/sponsor-12.png";
+import sponsor13 from "@/assets/sponsors/sponsor-13.png";
+import sponsor14 from "@/assets/sponsors/sponsor-14.png";
+import sponsor15 from "@/assets/sponsors/sponsor-15.png";
+import sponsor16 from "@/assets/sponsors/sponsor-16.png";
+import sponsor17 from "@/assets/sponsors/sponsor-17.png";
+import sponsor18 from "@/assets/sponsors/sponsor-18.png";
+import sponsor19 from "@/assets/sponsors/sponsor-19.png";
+import sponsor20 from "@/assets/sponsors/sponsor-20.png";
+
 export const speakerGrads = [
   "from-[oklch(0.78_0.2_40)] to-[oklch(0.65_0.22_15)]",
   "from-[oklch(0.7_0.2_320)] to-[oklch(0.55_0.2_290)]",
@@ -10,58 +52,58 @@ export const speakerGrads = [
 ];
 
 export const homeSpeakers = [
-  { n: "Founder", r: "FOSSASIA", img: "src/assets/speakers/speaker-1.jpg" },
-  { n: "Community Manager", r: "Outreachy", img: "src/assets/speakers/speaker-2.jpg" },
-  { n: "3X Founder", r: "FE TV HUB", img: "src/assets/speakers/speaker-3.jpg" },
-  { n: "Ex-Google", r: "Investor", img: "src/assets/speakers/speaker-4.jpg" },
-  { n: "Co-founder, CEO", r: "zeroone", img: "src/assets/speakers/speaker-5.jpg" },
-  { n: "Principal Ambassador", r: "Polkadot", img: "src/assets/speakers/speaker-6.jpg" },
-  { n: "Founder", r: "GirlScript Foundation", img: "src/assets/speakers/speaker-7.jpg" },
-  { n: "Talent Head", r: "BeInCrypto", img: "src/assets/speakers/speaker-8.jpg" },
+  { n: "Founder", r: "FOSSASIA", img: speaker1 },
+  { n: "Community Manager", r: "Outreachy", img: speaker2 },
+  { n: "3X Founder", r: "FE TV HUB", img: speaker3 },
+  { n: "Ex-Google", r: "Investor", img: speaker4 },
+  { n: "Co-founder, CEO", r: "zeroone", img: speaker5 },
+  { n: "Principal Ambassador", r: "Polkadot", img: speaker6 },
+  { n: "Founder", r: "GirlScript Foundation", img: speaker7 },
+  { n: "Talent Head", r: "BeInCrypto", img: speaker8 },
 ];
 
 export const allSpeakers = [
-  { n: "Founder", r: "FOSSASIA", img: "src/assets/speakers/speaker-1.jpg" },
-  { n: "Community Manager", r: "Outreachy", img: "src/assets/speakers/speaker-2.jpg" },
-  { n: "3X Founder", r: "FE TV HUB", img: "src/assets/speakers/speaker-3.jpg" },
-  { n: "Ex-Google", r: "Investor", img: "src/assets/speakers/speaker-4.jpg" },
-  { n: "Co-founder, CEO", r: "zeroone", img: "src/assets/speakers/speaker-5.jpg" },
-  { n: "Principal Ambassador", r: "Polkadot", img: "src/assets/speakers/speaker-6.jpg" },
-  { n: "Founder", r: "GirlScript Foundation", img: "src/assets/speakers/speaker-7.jpg" },
-  { n: "Talent Head", r: "BeInCrypto", img: "src/assets/speakers/speaker-8.jpg" },
-  { n: "Developer Advocate", r: "@DigitalOcean", img: "src/assets/speakers/speaker-9.jpg" },
-  { n: "Head of Events", r: "SCRIB3", img: "src/assets/speakers/speaker-10.jpg" },
-  { n: "Software Developer", r: "Zomato", img: "src/assets/speakers/speaker-11.jpg" },
-  { n: "Founder", r: "SI1ICON", img: "src/assets/speakers/speaker-12.jpg" },
-  { n: "Software Engineer II", r: "@Red Hat", img: "src/assets/speakers/speaker-13.jpg" },
-  { n: "Buidling cabal", r: "Arbitrum Fndn", img: "src/assets/speakers/speaker-14.jpg" },
-  { n: "CMO", r: "Soonami Venture", img: "src/assets/speakers/speaker-15.jpg" },
-  { n: "Software Engineer II", r: "Intuit", img: "src/assets/speakers/speaker-16.jpg" },
-  { n: "Ex-Ecosystem", r: "Brain Strom", img: "src/assets/speakers/speaker-17.jpg" },
-  { n: "Head of Product marketing", r: "Telegram", img: "src/assets/speakers/speaker-18.jpg" },
+  { n: "Founder", r: "FOSSASIA", img: speaker1 },
+  { n: "Community Manager", r: "Outreachy", img: speaker2 },
+  { n: "3X Founder", r: "FE TV HUB", img: speaker3 },
+  { n: "Ex-Google", r: "Investor", img: speaker4 },
+  { n: "Co-founder, CEO", r: "zeroone", img: speaker5 },
+  { n: "Principal Ambassador", r: "Polkadot", img: speaker6 },
+  { n: "Founder", r: "GirlScript Foundation", img: speaker7 },
+  { n: "Talent Head", r: "BeInCrypto", img: speaker8 },
+  { n: "Developer Advocate", r: "@DigitalOcean", img: speaker9 },
+  { n: "Head of Events", r: "SCRIB3", img: speaker10 },
+  { n: "Software Developer", r: "Zomato", img: speaker11 },
+  { n: "Founder", r: "SI1ICON", img: speaker12 },
+  { n: "Software Engineer II", r: "@Red Hat", img: speaker13 },
+  { n: "Buidling cabal", r: "Arbitrum Fndn", img: speaker14 },
+  { n: "CMO", r: "Soonami Venture", img: speaker15 },
+  { n: "Software Engineer II", r: "Intuit", img: speaker16 },
+  { n: "Ex-Ecosystem", r: "Brain Strom", img: speaker17 },
+  { n: "Head of Product marketing", r: "Telegram", img: speaker18 },
 ];
 
 export const pastSponsors = [
-  { n: "Devfolio", cat: "Hackathon Platform", img: "src/assets/sponsors/sponsor-1.png" },
-  { n: "GitHub", cat: "Developer Platform", img: "src/assets/sponsors/sponsor-2.png" },
-  { n: "QuillAudits", cat: "Security", img: "src/assets/sponsors/sponsor-3.png" },
-  { n: "Filecoin", cat: "Web3 Storage", img: "src/assets/sponsors/sponsor-4.png" },
-  { n: "Soonam.ai", cat: "Platform", img: "src/assets/sponsors/sponsor-5.png" },
-  { n: "5ireChain", cat: "Blockchain", img: "src/assets/sponsors/sponsor-6.png" },
-  { n: "Progate", cat: "Education", img: "src/assets/sponsors/sponsor-7.png" },
-  { n: "Dmail Network", cat: "Web3 Email", img: "src/assets/sponsors/sponsor-8.png" },
-  { n: "Suku", cat: "Platform", img: "src/assets/sponsors/sponsor-9.png" },
-  { n: "U°Community", cat: "Community", img: "src/assets/sponsors/sponsor-10.png" },
-  { n: "Oasis Network", cat: "Blockchain", img: "src/assets/sponsors/sponsor-11.png" },
-  { n: "HECO Chain", cat: "Blockchain", img: "src/assets/sponsors/sponsor-12.png" },
-  { n: "Core DAO", cat: "Blockchain", img: "src/assets/sponsors/sponsor-13.png" },
-  { n: "Socrates", cat: "Platform", img: "src/assets/sponsors/sponsor-14.png" },
-  { n: "Mentro", cat: "Community", img: "src/assets/sponsors/sponsor-15.png" },
-  { n: "Starlay Finance", cat: "DeFi", img: "src/assets/sponsors/sponsor-16.png" },
-  { n: "Floxus", cat: "Platform", img: "src/assets/sponsors/sponsor-17.png" },
-  { n: "QuickBlox", cat: "Communication", img: "src/assets/sponsors/sponsor-18.png" },
-  { n: "Reskill", cat: "Education", img: "src/assets/sponsors/sponsor-19.png" },
-  { n: "MongoDB", cat: "Database", img: "src/assets/sponsors/sponsor-20.png" },
+  { n: "Devfolio", cat: "Hackathon Platform", img: sponsor1 },
+  { n: "GitHub", cat: "Developer Platform", img: sponsor2 },
+  { n: "QuillAudits", cat: "Security", img: sponsor3 },
+  { n: "Filecoin", cat: "Web3 Storage", img: sponsor4 },
+  { n: "Soonam.ai", cat: "Platform", img: sponsor5 },
+  { n: "5ireChain", cat: "Blockchain", img: sponsor6 },
+  { n: "Progate", cat: "Education", img: sponsor7 },
+  { n: "Dmail Network", cat: "Web3 Email", img: sponsor8 },
+  { n: "Suku", cat: "Platform", img: sponsor9 },
+  { n: "U°Community", cat: "Community", img: sponsor10 },
+  { n: "Oasis Network", cat: "Blockchain", img: sponsor11 },
+  { n: "HECO Chain", cat: "Blockchain", img: sponsor12 },
+  { n: "Core DAO", cat: "Blockchain", img: sponsor13 },
+  { n: "Socrates", cat: "Platform", img: sponsor14 },
+  { n: "Mentro", cat: "Community", img: sponsor15 },
+  { n: "Starlay Finance", cat: "DeFi", img: sponsor16 },
+  { n: "Floxus", cat: "Platform", img: sponsor17 },
+  { n: "QuickBlox", cat: "Communication", img: sponsor18 },
+  { n: "Reskill", cat: "Education", img: sponsor19 },
+  { n: "MongoDB", cat: "Database", img: sponsor20 },
 ];
 
 export function getInitials(name: string) {
